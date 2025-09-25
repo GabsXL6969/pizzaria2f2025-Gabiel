d8372782411a1c17780392bfbff074aa4ca36f63
GabsXL6969 <rm95287@estudante.fieb.edu.br>
2025-09-25 07:47:50 -0300

    Consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,13 +1,27 @@
+// ferramenta de consumo de rotas (endpoints) do backend
 import axios from "axios"
+// Hooks do react - estado e renderização
+import { useState, useEffect } from "react"
+
 const Produtos = () => {
+    // Estado com listas de pizza
+    const [pizzas, setPizzas] = useState(['Calabresa', 'Muçarela', 'Baiana'])
+
     // Consumo de um endpoint com as pizzas
+    // GET - busca os dados do endpoint ou rota
+    // THEN - é uma Promise - método assíncrono
+    // catch - tratamento de erros
+    UseEffect(()=>{
     axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
     .then(response=>{
-        console.log(response.data)
+        console.log(response.data.data)
+        setPizzas(response.data.data)
     })
+    .catch(errors=>console.log(errors))
+},[])
 
     // Iteração da lista de pizzas
-    const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
+    const listaPizzas = pizzas.map(pizza=><li>{pizza.nome}</li>)
 
     return (
         <div>
