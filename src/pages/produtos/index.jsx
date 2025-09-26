c75176e8bfe80ff2750820e05261b852ea4980ae
GabsXL6969 <rm95287@estudante.fieb.edu.br>
2025-09-26 08:29:21 -0300

    sabão

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -4,21 +4,21 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 
 const Produtos = () => {
-    // Estado com listas de pizza
-    const [pizzas, setPizzas] = useState(['Calabresa', 'Muçarela', 'Baiana'])
+    // Estado com lista de pizzas
+    const [pizzas, setPizzas] = useState(['Calabraza', 'Muçarela', 'Baiana'])
 
     // Consumo de um endpoint com as pizzas
     // GET - busca os dados do endpoint ou rota
     // THEN - é uma Promise - método assíncrono
     // catch - tratamento de erros
-    UseEffect(()=>{
-    axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
-    .then(response=>{
-        console.log(response.data.data)
-        setPizzas(response.data.data)
-    })
-    .catch(errors=>console.log(errors))
-},[])
+    useEffect(()=>{
+        axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+        .then(response=>{
+            console.log(response.data.data)
+            setPizzas(response.data.data)
+        })
+        .catch(errors=>console.log(errors))
+    },[])
 
     // Iteração da lista de pizzas
     const listaPizzas = pizzas.map(pizza=><li>{pizza.nome}</li>)
